2481a2a57cd64671d15ebd4763747d5fb70c597c
rickryan30 <rickryan29.rr@gmail.com>
2023-09-04 19:16:04 +0800

    version 2.0.1

--- js/adapt-text-widow.js
+++ js/adapt-text-widow.js
@@ -17,168 +17,71 @@ define([
 		},
 
 		render: function() {
-			var articleEl = this.model.get('_articleView').$el;
-			console.log(articleEl);
-			const modelArticle = Array.from(articleEl);
-			var articleArr = [];
+			var articleElement = this.model.get('_articleView').$el;
+			const articleModel = this.model.get('_articleView').model;
 
-			modelArticle.forEach(elementArticle => {
-				// article
-				const dataArticle = Array.from(elementArticle.children);
-
-				dataArticle.forEach(elementInner => {
-					// article-inner
-					const dataInner = Array.from(elementInner.firstElementChild.children);
-					dataInner.forEach(elementTitBod => {
-						// article-title/body/instruction
-						const dataTitBod = Array.from(elementTitBod.children);
-						dataTitBod.forEach(elementTitBodIn => {
-							// article-title/body/instruction-inner
-							const dataTitBodIn = Array.from(elementTitBodIn.children);
-							if (dataTitBodIn.length > 0) {
-								dataTitBodIn.forEach(elementChild => {
-									const dataChild = Array.from(elementChild.children);
-									if (dataChild.length > 0) {
-										dataChild.forEach(elementChildIn => {
-											console.log("." + elementChildIn.parentElement.parentElement.className + " " + elementChildIn.parentElement.localName + " " + elementChildIn.localName);
-										});
-									} else {
-										console.log("." + elementChild.parentElement.className + " " + elementChild.localName);
-									}
-								});
-							} else {
-								console.log("." + elementTitBodIn.classList[0]);
-							}
-						});
-					});
-				});
-
-				// console.log(articleArr);
-				// const articleViewResult = this.removeDuplicates(articleArr);
-				// console.log(articleViewResult);
-				// articleViewResult.forEach(elementArticleView => {
-				// 	const dataArticleView = $(elementArticleView, $article);
-				// 	return dataArticleView.length > 0 ? this.checkTextWidow(dataArticleView) : '';
-				// });
-			});
+			const checkEmptyArr = articleModel.get('_classes').split(' ');
+			const hasAnyEmptyElement = checkEmptyArr.filter(item => item);
+			var includesArr = hasAnyEmptyElement.length > 0 && hasAnyEmptyElement.includes("disabled-textwidow");
+			if (includesArr !== true) {
+				$("[class$='-title'] > [class^='article']", articleElement).length > 0 && this.checkTextWidow($("[class$='-title'] > [class^='article']", articleElement));
+				$("[class$='-body'] > [class^='article'] > p", articleElement).length > 0 && this.checkTextWidow($("[class$='-body'] > [class^='article'] > p", articleElement));
+				$("[class$='-body'] > [class^='article'] > ul li", articleElement).length > 0 && this.checkTextWidow($("[class$='-body'] > [class^='article'] > ul li", articleElement));
+				$("[class$='-body'] > [class^='article'] > ol li", articleElement).length > 0 && this.checkTextWidow($("[class$='-body'] > [class^='article'] > ol li", articleElement));
+				$("[class$='-instruction'] > [class^='article']", articleElement).length > 0 && this.checkTextWidow($("[class$='-instruction'] > [class^='article']", articleElement));
+			}
 		},
 
 		onblockViewRendered: function(view) {
-				const $parentArticleId = view.model.findAncestor('articles').get('_id');
-				const $childArticleId = this.model.get('_articleView').model.get('_id');
+			const $articleParentId = view.model.findAncestor('articles').get('_id');
+			const $articleChildId = this.model.get('_articleView').model.get('_id');
 
-				if ($parentArticleId === $childArticleId) {
-
-					this.model.get('_blockViews').push(view);
-					const modelBlock = this.model.get('_blockViews');
-					var blockArr = [];
-
-					modelBlock.forEach(elementBlock => {
-						// block-inner
-						const dataBlock = Array.from(elementBlock.$el.context.firstElementChild.children);
-						dataBlock.forEach(elementTitleBlock => {
-							// block-title/body/instruction component-container
-							const dataTitleBlockCls = elementTitleBlock.className;
-							if (dataTitleBlockCls != 'component-container') {
-								const dataTitleBlock = Array.from(elementTitleBlock.children);
-								dataTitleBlock.forEach(elementTitleBlockIn => {
-									// block-title/body/instruction-inner component-container
-									const dataTitleBlockIn = Array.from(elementTitleBlockIn.children);
-									if (dataTitleBlockIn.length > 0) {
-										dataTitleBlockIn.forEach(elementBlockChild => {
-											 const dataBlockChild = Array.from(elementBlockChild.children);
-											 if (dataBlockChild.length > 0) {
-												dataBlockChild.forEach(elementBlockChildIn => {
-													blockArr.push("." + elementBlockChildIn.parentElement.parentElement.className + " " + elementBlockChildIn.parentElement.localName + " " + elementBlockChildIn.localName);
-												});
-											} else {
-												blockArr.push("." + elementBlockChild.parentElement.className + " " + elementBlockChild.localName);
-											}
-										});
-									} else {
-										blockArr.push("." + elementTitleBlockIn.classList[0]);
-									}
-									
-								});
-							}
-						});
-						// const blockViewResult = this.removeDuplicates(blockArr);
-						// blockViewResult.forEach(elementBlockView => {
-						// 	const dataBlockView = $(elementBlockView, view.$el);
-						// 	return dataBlockView.length > 0 ? this.checkTextWidow(dataBlockView) : null;
-						// });
-					});
-
+			if ($articleParentId === $articleChildId) {
+				this.model.get('_blockViews').push(view);
+				const checkEmptyArr = view.model.get('_classes').split(' ');
+				const hasAnyEmptyElement = checkEmptyArr.filter(item => item);
+				var includesArr = hasAnyEmptyElement.length > 0 && hasAnyEmptyElement.includes("disabled-textwidow");
+				if (includesArr !== true) {
+					$("[class$='-inner'] > [class$='-title'] > [class^='block']", view.$el).length > 0 && this.checkTextWidow($("[class$='-inner'] > [class$='-title'] > [class^='block']", view.$el));
+					$("[class$='-body'] > [class^='block'] > p", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > [class^='block'] > p", view.$el));
+					$("[class$='-body'] > [class^='block'] > ul li", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > [class^='block'] > ul li", view.$el));
+					$("[class$='-body'] > [class^='block'] > ol li", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > [class^='block'] > ol li", view.$el));
 				}
-			},
+			}
+		},
 			
 		onComponentViewRendered: function(view) {
-				var $parentArticleId = view.model.findAncestor('articles').get('_id');
-				var $childArticleId = this.model.get('_articleView').model.get('_id');
+			var $articleParentId = view.model.findAncestor('articles').get('_id');
+			var $articleChildId = this.model.get('_articleView').model.get('_id');
+
+			if ($articleParentId === $articleChildId) {
+				this.model.get('_componentViews').push(view);
+				const checkEmptyArr = view.model.get('_classes').split(' ');
+				const hasAnyEmptyElement = checkEmptyArr.filter(item => item);
+				var includesArr = hasAnyEmptyElement.length > 0 && hasAnyEmptyElement.includes("disabled-textwidow");
 				
-				if ($parentArticleId === $childArticleId) {
-
-					this.model.get('_componentViews').push(view);
-					
-					if (view.model.get('_component') !== 'blank') {
-
-						const modelComponent = this.model.get('_componentViews');
-						var componentArr = [];
-
-						// modelComponent.forEach(elementComponent => {
-						// 	// component result
-						// 	const dataComponent = Array.from(elementComponent.$el.context.firstElementChild.children);
-							
-						// 	if (dataComponent.length > 1) {
-						// 		console.log(dataComponent);
-						// 	}
-						// 	dataComponent.forEach(elementHeadWidgt => {
-						// 		// component header - widget
-						// 		const dataHeadWidgt = Array.from(elementHeadWidgt.children);
-						// 		dataHeadWidgt.forEach(elementHeadWidgtIn => {
-						// 			// component header-inner - 
-						// 			const dataHeadWidgtIn = Array.from(elementHeadWidgtIn.children);
-						// 			dataHeadWidgtIn.forEach(elementTitleBody => {
-						// 				// component-title/body/instruction items
-						// 				const dataTitleBody = Array.from(elementTitleBody.children);
-						// 				dataTitleBody.forEach(elementTitleBodyIn => {
-						// 					// component-title/body/instruction-inner items-inner
-						// 					const dataTitleBodyIn = Array.from(elementTitleBodyIn.children);
-						// 					if (dataTitleBodyIn.length > 0) {
-						// 						dataTitleBodyIn.forEach(elementComponetChild => {
-						// 							const dataComponentChild = Array.from(elementComponetChild.children);
-						// 							if (dataComponentChild.length > 0) {
-						// 								dataComponentChild.forEach(elementComponetChildIn => {
-						// 									console.log("." + elementComponetChildIn.parentElement.parentElement.className + " " + elementComponetChildIn.parentElement.localName + " " + elementComponetChildIn.localName);
-						// 									componentArr.push("." + elementComponetChildIn.parentElement.parentElement.className + " " + elementComponetChildIn.parentElement.localName + " " + elementComponetChildIn.localName);
-						// 								});
-						// 							} else {
-						// 								console.log("." + elementComponetChild.parentElement.className + " " + elementComponetChild.localName);
-						// 								componentArr.push("." + elementComponetChild.parentElement.className + " " + elementComponetChild.localName);
-						// 							}
-						// 						});	
-						// 					} else {
-						// 						// console.log(elementTitleBodyIn.classList);
-						// 						console.log("." + elementTitleBodyIn.classList[0]);
-						// 						componentArr.push("." + elementTitleBodyIn.classList[0]);
-						// 					}
-						// 				});
-						// 			});
-						// 		});
-						// 	});
-							
-						// 	// const compoenentViewResult = this.removeDuplicates(componentArr);
-						// 	// console.log(componentArr);
-						// 	// console.log(compoenentViewResult);
-						// 	// compoenentViewResult.forEach(elementComponentView => {
-						// 	// 	const dataComponentView = $(elementComponentView, view.$el);
-						// 	// 	return dataComponentView.length > 0 ? this.checkTextWidow(dataComponentView) : '';
-						// 	// });
-						// });
+				if (view.model.get('_component') !== 'blank' && includesArr !== true) {
+					if ($("[class$='-title']", view.$el).length > 0) {
+						$("[class$='-title'] > [class$='-inner']", view.$el).length > 0 ? this.checkTextWidow($("[class$='-title'] > [class$='-inner']", view.$el)) : this.checkTextWidow($("[class$='-title']", view.$el));
 					}
-				}
-			},
+					
+					if($("[class$='-body'] > [class$='-inner']", view.$el).length > 0) {
+						$("[class$='-body'] > [class$='-inner'] > p", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > [class$='-inner'] > p", view.$el));
+						$("[class$='-body'] > [class$='-inner'] > ul li", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > [class$='-inner'] > ul li", view.$el));
+						$("[class$='-body'] > [class$='-inner'] > ol li", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > [class$='-inner'] > ol li", view.$el));
+					}
+					
+					if ($("[class$='-body']", view.$el).length > 0) {
+						$("[class$='-body'] > p", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > p", view.$el));
+						$("[class$='-body'] > ul li", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > ul li", view.$el));
+						$("[class$='-body'] > ol li", view.$el).length > 0 && this.checkTextWidow($("[class$='-body'] > ol li", view.$el));
+					}
 
+					$("[class$='-instruction'] > [class$='-inner']", view.$el).length > 0 && this.checkTextWidow($("[class$='-instruction'] > [class$='-inner']", view.$el));
+				}
+			}
+		},
+			  
 			checkTextWidow: function(textWidowValue) {
 				textWidowValue.each(function(){
 					var trimSpaces = $(this).html().trim();
@@ -194,10 +97,10 @@ define([
 				});
 			},
 
-			// removeDuplicates: function(arr) {
-			// 	return arr.filter((item,
-			// 		index) => arr.indexOf(item) === index);
-			// },
+			removeDuplicates: function(arr) {
+				return arr.filter((item,
+					index) => arr.indexOf(item) === index);
+			},
 
 			onRemove: function() {
 				this.model.get('_articleView').$el.removeClass('text-widow');
